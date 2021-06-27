b439f848537c4249a8578ae1fafa4cde55f6b31f
esrakarakis <esra.cakir@useinsider.com>
2021-06-27 14:25:12 +0300

    Change request fixes

--- odev5/main.py
+++ odev5/main.py
@@ -3,11 +3,11 @@ from selenium.webdriver.common.by import By
 
 class Homework:
     NEWSLETTER_UTM_SETTINGS = (By.ID, "enableUTMSettings_")
-    INPUT_WARNING_MESSAGE = (By.CSS_SELECTOR, "in-text-input-wrapper__error-message")
+    INPUT_WARNING_MESSAGE = (By.CLASS_NAME, "in-text-input-wrapper__error-message")
     JOURNEY_DO_LIST_FILTER_INPUT = (By.ID, "search-value")
-    TEST_BUTTON_RADIO = (By.CSS_SELECTOR, "in-radio-button-wrapper__label")  # 5
-    JOURNEY_STATISTICS = (By.CSS_SELECTOR, "statistics")  # 0
+    TEST_BUTTON_RADIO = (By.ID, "Test")
+    JOURNEY_STATISTICS = (By.CSS_SELECTOR, 'tr[item-index="0"] td.vuetable-slot.statistics')
     MESSAGE_BOX = (By.ID, "delete")
-    JOURNEY_REMOVE_CHANGE = (By.CSS_SELECTOR, "py-2")
-    ALERT_TOASTER = (By.CSS_SELECTOR, "messageAlertBoxHidden")
+    JOURNEY = (By.LINK_TEXT, "SMS")
+    ALERT_TOASTER = (By.CLASS_NAME, "messageAlertBoxHidden")
     SOCIAL_PROOF_CREATE = (By.ID, "create-mobile-campaign")
